1bdda611f41549f942707c26a781b4346f27db39
MoahmedElsaed <ashraf@orcas.io>
2022-04-07 23:33:34 +0200

    add project modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { UrlCheckModule } from './url-check/url-check.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/health-checker')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/health-checker'),
+    UserModule,
+    UrlCheckModule,
+    ReportsModule,
+  ],
   controllers: [],
   providers: [],
 })
